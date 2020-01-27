8eb6d5d79c3f35472ca6457b6c1d54b423a1a74b
Adrien Deblock <adrien.deblock@davidson.fr>
2020-01-27 10:14:42 +0100

    feat: clever-cloud implementation

--- src/test/java/com/miage/altea/tp/trainer_api/repository/TrainerRepositoryTest.java
+++ src/test/java/com/miage/altea/tp/trainer_api/repository/TrainerRepositoryTest.java
@@ -5,6 +5,7 @@ import com.miage.altea.tp.trainer_api.bo.Pokemon;
 import com.miage.altea.tp.trainer_api.bo.Trainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.repository.CrudRepository;
 
@@ -13,6 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@AutoConfigureDataJpa
 class TrainerRepositoryTest {
 
     @Autowired
